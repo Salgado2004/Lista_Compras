e22a34d01ed4b898d54ce542bf2199668ef67729
Salgado2004 <you@example.com>
2023-01-16 21:52:53 -0300

    Função de adicionar novos itens

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-adicionar = document.querySelector("#add");
+novo = document.querySelector("#add");
 
-adicionar.onclick = function(e) {
+novo.onclick = function(e) {
     janela = document.querySelector("#janela");
     main = document.querySelector("#mainContent");
 
     main.style.filter = "blur(3px)";
     janela.style.display = "block";
-    adicionar.disabled = true;
+    novo.disabled = true;
 }
 
 fecharJanela = document.querySelector("#fechar");
 
 fecharJanela.onclick = function(e){
     janela = document.querySelector("#janela");
+    form = document.querySelector("form");
     main = document.querySelector("#mainContent");
 
     main.style.filter = "blur(0)";
     janela.style.display = "none";
-    adicionar.disabled = false;
+    novo.disabled = false;
+    form.reset();
 }
+
+adicionar = document.querySelector("#submit");
+
+adicionar.onclick = function(e){
+    e.preventDefault();
+
+    lista = document.querySelector("#lista");
+
+    nome = document.querySelector("#nome").value;
+    qtd = document.querySelector("#qtd").value;
+    medida = document.querySelector("#medida").value;
+
+    lista.innerHTML = lista.innerHTML + `<ul>${nome}   ${qtd} ${medida}</ul>`;
+
+    main.style.filter = "blur(0)";
+    janela.style.display = "none";
+    novo.disabled = false;
+    form.reset();
+}
+
